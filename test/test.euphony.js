33d41d3423aef0a701679a5adbfb27e3dd58efe1
Ji-woong Choi <hello.designe@facebook.com>
2019-12-10 00:50:22 +0900

    added Unit Test based on Mocha

--- test/test.euphony.js
+++ test/test.euphony.js
@@ -1,13 +1,47 @@
 var expect = chai.expect;
 
-
-describe('#MY_EUPHONY_TEST', function () {
-    let euphy = new Euphony();
+let euphy = new Euphony();
+describe('#Euphony Setting Test', function () {
+    let expected = NaN;
     it('Channel Setting', function () {
-        let euphy = new myEuphony();
         euphy.setChannel(1);
-        let expected = euphy.CHANNEL;
+        expected = euphy.CHANNEL;
+        expect(expected).to.equal(1);
 
+        euphy.setChannel(2);
+        expected = euphy.CHANNEL;
+        expect(expected).to.equal(2);
+    });
+
+    it('Modulation Setting', function () {
+        euphy.setModulation("ASK");
+        expected = euphy.MODULATION_TYPE;
+        expect(expected).to.equal(0);
+
+        euphy.setModulation("FSK");
+        expected = euphy.MODULATION_TYPE;
+        expect(expected).to.equal(1);
+
+        euphy.setModulation("CPFSK");
+        expected = euphy.MODULATION_TYPE;
+        expect(expected).to.equal(2);
+    });
+
+    it('Base Frequency Setting', function () {
+        euphy.setBaseFrequency(19000);
+        expected = euphy.BASE_FREQUENCY;
+        expect(expected).to.equal(19000);
+    });
+});
+
+describe('#Euphony Error Correction', function() {
+    let expected = NaN;
+    it('Checksum', function() {
+        expected = euphy.makeChecksum("abcdef");
+        expect(expected).to.equal(5);
+    });
+    it('Parity Check', function() {
+        expected = euphy.makeParallelParity("abcdef");
         expect(expected).to.equal(1);
     });
 });
