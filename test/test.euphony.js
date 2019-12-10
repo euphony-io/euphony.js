3103402a427a330aaacb48a69efaabd938658cf6
designe <hello.designe@facebook.com>
2019-12-10 10:42:21 +0900

    Added UnitTest Case

--- test/test.euphony.js
+++ test/test.euphony.js
@@ -32,6 +32,30 @@ describe('#Euphony Setting Test', function () {
         expected = euphy.BASE_FREQUENCY;
         expect(expected).to.equal(19000);
     });
+    
+    it('Buffer Size Setting', function () {
+        // DEFAULT CHECK
+        expect(euphy.outBuffer).to.be.an.instanceOf(Array);
+        expect(euphy.outBuffer[0]).to.have.lengthOf(2048);
+        expect(euphy.outBuffer).to.be.an('array').that.is.not.empty;
+        expect(euphy.outBuffer).to.have.lengthOf(32);
+        
+        euphy.setBufferSize(44100); // 1 sec
+        expected = euphy.BUFFERSIZE;
+        expect(expected).to.equal(44100);
+
+        // After Setting
+        expect(euphy.outBuffer).to.be.an.instanceOf(Array);
+        expect(euphy.outBuffer).to.be.an('array').that.is.not.empty;
+        expect(euphy.outBuffer).to.have.lengthOf(32);
+        expect(euphy.outBuffer[0]).to.have.lengthOf(44100);
+        
+    });
+
+    it('Setting Input Data', function() {
+        euphy.setCode("Euphony is acoustic data communication library");
+        expect(euphy.playBufferIdx).to.equal(96);
+    });
 });
 
 describe('#Euphony Error Correction', function() {
